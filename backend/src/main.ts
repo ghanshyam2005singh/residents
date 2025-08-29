16fe9802207530f5adc8cd74f2a6127c43f23327
Ghanshyam Singh <ghanshyam2005singh@gmail.com>
2025-08-29 18:37:55 +0530

    update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000', // local frontend
-      'https://your-frontend-url.com', // deployed frontend
+      'https://residents-atjo.onrender.com', // deployed frontend
     ],
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     allowedHeaders: [
